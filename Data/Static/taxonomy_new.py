f386770c6c41e6505acda4c6a12edeb98f097d7f
Risto Trajanov <risto.trajanov@gmail.com>
2023-05-03 21:29:21 -0500

    Data

---
+++ Data/Static/taxonomy_new.py
@@ -0,0 +1,69 @@
+
+# Environmental key words
+
+environmental_keys = [
+    'climate change',
+    'carbon emissions',
+    'water pollution',
+    'air pollution',
+    'biodiversity',
+    'deforestation',
+    'reforestation',
+    'low carbon',
+    'plastics',
+    'solar power',
+    'carbon neutral',
+    'chemicals'
+]
+
+social_keys = [
+    'customer satisfaction',
+    'data protection',
+    'data privacy',
+    'gender',
+    'diversity',
+    'human rights',
+    'labor standards',
+    'discrimination',
+    'donation',
+    'community',
+    'volunteer',
+    'working condition',
+    'health care',
+    'charity'
+]
+
+governence_keys = [
+    'illegal',
+    'bribery',
+    'corruption',
+    'lobbying',
+    'whistleblower'
+    'money laundring'
+    'antitrust',
+    'lawsuit'
+]
+
+investing_keys = [
+    'esg investing'
+    'sustainability investing',
+    'esg regulations',
+    'greenwashing',
+    'esg'
+    
+]
+
+
+esg_dict = {
+    'environmental': environmental_keys,
+    'social': social_keys,
+    'governence': governence_keys,
+    'investing': investing_keys
+}
+
+tag_dict = {
+    'ENV': environmental_keys,
+    'SOC': social_keys,
+    'CORGOV': governence_keys,
+    'INV': investing_keys
+}
